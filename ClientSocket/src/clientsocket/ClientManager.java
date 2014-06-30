84b7cb17bf15ffcb08b52409a4f5534295cd1b6c
Ifeanyi Agu <yankitwizzy@gmail.com>
2014-06-29 20:56:09 -0700

    updated client

--- ClientSocket/src/clientsocket/ClientManager.java
+++ ClientSocket/src/clientsocket/ClientManager.java
@@ -13,64 +13,99 @@ import java.net.Socket;
 import java.util.Scanner;
 
 /**
- *
+ * 
  * @author Ali
  */
 public class ClientManager {
 
-    private final Socket socket;
-    private InputStream inStream;
-    private OutputStream outStream;
-    private Scanner in;
-    private PrintWriter out;
-    Scanner userInputScanner = new Scanner(System.in);
+	private final Socket socket;
+	private InputStream inStream;
+	private OutputStream outStream;
+	private Scanner in;
+	private PrintWriter out;
+	Scanner userInputScanner = new Scanner(System.in);
 
-    public ClientManager(Socket socket) throws IOException {
-        this.socket = socket;
-        this.setUps();
-        this.clientRun();
-        this.closeConnection();
-    }
-    
-    private void clientRun() {
-        String userInput = "dummi";
-        while(!userInput.equals("close")){
-            System.out.println("Enter command:");
-            userInput = userInputScanner.nextLine();
-            sendRequest(userInput);
-            receiveResponse();
-        }
-    }
-    private void setUps() throws IOException {
-        inStream = socket.getInputStream();
-        outStream = socket.getOutputStream();
-        in = new Scanner(inStream);
-        out = new PrintWriter(outStream);
-    }
+	public ClientManager(Socket socket) throws IOException {
+		this.socket = socket;
+		this.setUps();
+		this.clientRun();
+		this.closeConnection();
+	}
 
-    private void sendRequest(String request) {
-        out.print(request + "\n");
-        out.flush();
-        System.out.println("Sending: " + request);
-    }
+	private void clientRun() {
+		String userInput = "yanki4";
+		// I modified this line
+		sendRequest(userInput);
+		// modification ended here
 
-    private void receiveResponse() {
-        System.out.println("\n Server Response:");
-        while (in.hasNextLine()) {
-            String input = in.nextLine();
-            if (input.equals("end")) {
-                break;
-            } else {
-                System.out.println(input);
-            }
-        }
-    }
+		while (!userInput.equals("close")) {
+			System.out.println("\nEnter command:");
+			userInput = userInputScanner.nextLine();
 
-    private void closeConnection() throws IOException {
-       in.close();
-       out.close();
-       inStream.close();
-       outStream.close();
-       socket.close();
-    }
+			if (userInput.equals("follow")) {
+				sendRequest("follow");
+				requestStock();
+			} else {
+				sendRequest(userInput);
+				receiveResponse();
+			}
+		}
+	}
+
+	private void setUps() throws IOException {
+		inStream = socket.getInputStream();
+		outStream = socket.getOutputStream();
+		in = new Scanner(inStream);
+		out = new PrintWriter(outStream);
+	}
+
+	private void sendRequest(String request) {
+		out.print(request + "\n");
+		out.flush();
+		System.out.println("Sending: " + request);
+	}
+
+	private void receiveResponse() {
+		System.out.println("\n Server Response:");
+
+		if (in.hasNext()) {
+			while (in.hasNextLine()) {
+				String input = in.nextLine();
+				if (input.equals("end")) {
+					break;
+				} else {
+					System.out.println(input);
+				}
+			}
+
+		} else {
+			receiveResponse();
+		}
+
+	}
+
+	private void requestStock() {
+		System.out.println("Enter stock tickername or q to quit: ");
+		String userInput = userInputScanner.nextLine();
+		while (true) {
+			if (!userInput.equals("q")) {
+				sendRequest(userInput);
+				receiveResponse();
+				System.out.println("Enter stock tickername or q to quit: ");
+				userInput = userInputScanner.nextLine();
+			} else {
+				sendRequest("reset");
+				receiveResponse();
+				break;
+			}
+		}
+	}
+
+	private void closeConnection() throws IOException {
+		in.close();
+		out.close();
+		inStream.close();
+		outStream.close();
+		socket.close();
+	}
 }
